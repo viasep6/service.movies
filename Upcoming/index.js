b98b9867ec64a6a196c893455314dbd91c0574b9
anderssonderby <anders.01@live.dk>
2021-12-02 21:37:30 +0100

    Add 'Popular' and 'Upcoming' functions

---
+++ Upcoming/index.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+module.exports = function (context, req) {
+    let config = {
+    method: 'get',
+    url: process.env["TMDB_API_URL"] + '/3/movie/upcoming',
+    headers: { 
+        'Authorization': process.env["TMDB_API_TOKEN"]
+    }};
+
+    return axios(config)
+    .then(function (response)
+    {
+        let filteredResponse = response.data.results.map(movie => {
+            return {
+                "id": movie.id,
+                "title": movie.title,
+                "release_date": movie.release_date
+            }
+        });
+
+        context.res = {
+            status: response.status,
+            body: filteredResponse,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    })
+    .catch(function (error)
+    {
+        context.res = {
+            status: 500,
+            body: error
+           }
+    });
+}
